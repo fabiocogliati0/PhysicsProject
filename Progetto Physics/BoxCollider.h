f987ce6a7729f1ed694cfc9584eddc19d7fb3e3c
Fabio Cogliati <fabiocogliati0@gmail.com>
2015-03-18 18:40:26 +0100

    Corretto un piccolo errorino sul BoxCollider

--- Progetto Physics/BoxCollider.h
+++ Progetto Physics/BoxCollider.h
@@ -9,7 +9,7 @@ public:
 
 	bool intersect(const Collider& i_other, float o_intersection[3]) const;	//do double dispatch
 
-protected:
+private:
 
 	bool intersectWho(const SphereCollider& i_other, float o_intersection[3]) const;
 	bool intersectWho(const BoxCollider& i_other, float o_intersection[3])const;
